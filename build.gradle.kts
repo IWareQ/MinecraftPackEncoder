f8b0c3bf6a8ddab9a3bbce391e6b9652ff316a9f
IWareQ <dimonpo96@mail.ru>
2023-03-27 11:20:29 +0400

    Fix startup

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 	id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
-group = "me.iwareq.mcperpencrdec"
+group = "me.iwareq.mpe"
 version = "1.0.0"
 
 repositories {
@@ -14,14 +14,20 @@ repositories {
 }
 
 dependencies {
-	api("org.bouncycastle:bcprov-jdk15on:1.70")
-	api("com.fasterxml.jackson.core:jackson-databind:2.14.2")
+	implementation("org.bouncycastle:bcprov-jdk15on:1.70")
+	implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
 }
 
 application {
-	mainClass.set("me.iwareq.mcperpencrdec.MainKt")
+	mainClass.set("me.iwareq.mpe.MainKt")
 }
 
 kotlin {
 	jvmToolchain(8)
+}
+
+tasks {
+	shadowJar {
+		archiveFileName.set("MinecraftPackEncoder-${version}.jar")
+	}
 }
