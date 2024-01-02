f042eb53be1c73162c9cae14137f4aa776663827
Dmitry Poyarkov <dimonpo96@mail.ru>
2024-01-03 01:49:02 +0400

    fix new packs decrypt

--- src/main/kotlin/me/iwareq/mpe/data/Contents.kt
+++ src/main/kotlin/me/iwareq/mpe/data/Contents.kt
@@ -1,7 +1,9 @@
 package me.iwareq.mpe.data
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 class Contents {
 
 	@JsonProperty("content")
